2ab3910dc5c534de34d0315d9545cd7e5b52f81c
justnotiyann <muhammadfitrian0712@gmail.com>
2022-09-09 17:20:01 +0700

    add new fresh style

--- public/javascript/script.js
+++
@@ -1,6 +0,0 @@
-let nama = "MUHAMMAD FITRIAN SHOUSYADE ALMADINA";
-let nim = 41215308;
-const button = document.querySelector("#alert");
-button.addEventListener("click", () => {
-  alert(`Halo javascript saya ${nama} nim ${nim} sedang belajar membuat web !`);
-});
